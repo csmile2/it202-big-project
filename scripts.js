0c886c68c722a64de8fc398c90a5e87c86fe4911
Clayton Smiley <37163784+csmile2@users.noreply.github.com>
2019-11-22 13:15:27 -0600

    instantiating api call and view

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 console.log("Using API https://newsapi.org/")
 // API: https://newsapi.org/
 // KEY: e65da836c72f4e8894103a8e401cff95
+$(document).ready(function(){
+    $.get("https://restcountries.eu/rest/v2/all", function(res){
+        $.each(res, function(u,v){
+            console.log(v);
+            $("#countriesView").append(
+                "<b>"+v.name+":</b></br>"
+            );
+        });
+
+    });
+})
