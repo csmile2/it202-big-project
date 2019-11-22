9b2fc3d0e8994e7d7d66b301cbb75bcd049d9fff
Clayton Smiley <37163784+csmile2@users.noreply.github.com>
2019-11-22 14:06:44 -0600

    UI changes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-console.log("Using API https://newsapi.org/")
+// console.log("Using API https://newsapi.org/")
 // API: https://newsapi.org/
 // KEY: e65da836c72f4e8894103a8e401cff95
+// API: https://restcountries.eu/rest/v2/all
+
 $(document).ready(function(){
     $.get("https://restcountries.eu/rest/v2/all", function(res){
         $.each(res, function(u,v){
