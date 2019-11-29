7a6094eefcd7e8abeb49a5c90a9bf980881c9314
Clayton Smiley <37163784+csmile2@users.noreply.github.com>
2019-11-29 14:20:45 -0600

    changing the card layout and adding functionality

--- scripts.js
+++ scripts.js
@@ -4,54 +4,45 @@
 // API: https://restcountries.eu/rest/v2/all
 
 function drawRegionCards(){
+    $("#countriesView").empty();
     var regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
-
     for(var i = 0; i < regions.length; i ++){
-
         var cardClone = $(".cardTemplate").clone();
         cardClone.find(".mdc-card__media-content").text(regions[i]);
         cardClone.removeClass("cardTemplate");
         cardClone.css("background-image", "url(images/"+regions[i]+".jpg)");
         $("#countriesView").append(cardClone);
+        cardClone.attr("id",regions[i]);
+        cardClone.on("click", function(){
+            console.log($(this).attr("id"));
+            $("#countriesView").empty();
+            drawByRegion($(this).attr("id"));
+        });
     }
 }
+
+function drawByRegion(region){
+    console.log("Going to fetch everything in: " + region);
+    var url = "https://restcountries.eu/rest/v2/region/"+region;
+    console.log("Fetching from this url" + url);
+    $.get(url, function(res){
+        $.each(res, function(u,v){
+            var cardClone = $(".cardTemplate").clone();
+            cardClone.find(".mdc-card__media-content").text(v.name);
+            cardClone.removeClass("cardTemplate");
+            console.log(v);
+            cardClone.css("background-image", "url("+v.flag+")");
+
+            $("#countriesView").append(cardClone);
+        });
+    });
+    
+}
+
 
 $(document).ready(function(){
 
     drawRegionCards();
-    // $.get("https://restcountries.eu/rest/v2/all", function(res){
-    //     $.each(res, function(u,v){
-    //         console.log(v);
-    //         var cardClone = $(".cardTemplate").clone();
-    //         cardClone.find(".mdc-card__media-content").text(v.name);
-    //         cardClone.removeClass("cardTemplate");
-    //         $("#countriesView").append(cardClone);
-    //     });
-    // });
 
 
-
-    // var regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
-    // var regionsLength = regions.length;
-    
-    // for(var i = 0; i < regionsLength; i++){
-    //     console.log(regions[i]);
-    //     var url = "https://restcountries.eu/rest/v2/region/"+regions[i];
-    //     console.log("Fetching from this url" + url);
-    //     $.get(url, function(res){
-    //         $.each(res, function(u,v){
-    //             // console.log(v);
-    //             var cardClone = $(".cardTemplate").clone();
-    //             cardClone.find(".mdc-card__media-content").text(v.name);
-    //             cardClone.removeClass("cardTemplate");
-    //             $("#countriesView").append(cardClone);
-    //         });
-    //     });
-    // }
-        
-    // region
-    // https://restcountries.eu/rest/v2/region/{region}
-    // Search by region: Africa, Americas, Asia, Europe, Oceania
-
-
-})
+});
