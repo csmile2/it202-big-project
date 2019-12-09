d14c761d490a2e0a34a84ad596a1398c35a04942
Clayton Smiley <37163784+csmile2@users.noreply.github.com>
2019-12-08 20:09:25 -0600

    additions to Explore feature

--- scripts.js
+++ scripts.js
@@ -40,25 +40,19 @@ function drawNewsModal(country){
 
 }
 
-function explore(country){
-    var url = "https://restcountries.eu/rest/v2/name/"+country;
-    var dialogClone = $("#ex1").clone();
-    $.get(url, function(res){
-        console.log(res[0]);
-        // $("h1#countryName").text(res[0]["name"]);
-        dialogClone.find("#countryName").text(res[0]["name"]);
-        dialogClone.find("#countryInfo").append(
-            "Capital: "+res[0]["capital"]+"<br>"+
-            "Population: "+res[0]["population"]+"<br>"+
-            "Currency: "+res[0]["currencies"][0]["name"]
-        );
-        // $("p#countryInfo").append(
-        //     "Capital: "+res[0]["capital"]+"<br>"+
-        //     "Population: "+res[0]["population"]+"<br>"+
-        //     "Currency: "+res[0]["currencies"][0]["name"]+"<br>"
-        //     );
-    });
-}
+// function explore(country){
+//     var url = "https://restcountries.eu/rest/v2/name/"+country;
+//     $.get(url, function(res){
+//         console.log(res[0]);
+//         dialogClone.find("h1#countryName").text(res[0]["name"]);
+//         dialogClone.find("#countryInfo").append(
+//             "Capital: "+res[0]["capital"]+"<br>"+
+//             "Population: "+res[0]["population"]+"<br>"+
+//             "Currency: "+res[0]["currencies"][0]["name"]
+//         );
+//     });
+     
+// }
 
 function drawByRegion(region){
     console.log("Going to fetch everything in: " + region);
@@ -78,12 +72,28 @@ function drawByRegion(region){
         $(".mdc-button__label:contains('View News')").on("click", function(){
             var country = $(this).closest(".mdc-card").attr("id");
             console.log("Viewing news for this country: " + country);
-            drawNewsModal(country);
+            // drawNewsModal(country);
         });
         $(".mdc-button__label:contains('Explore')").on("click", function(){
             var country = $(this).closest(".mdc-card").attr("id");
             console.log("Exploring : " + country);
-            explore(country);
+            // var contentStr = explore(country);
+            var url = "https://restcountries.eu/rest/v2/name/"+country;
+            card = $(this).closest(".mdc-card");
+            dialogClone = $("#ex1").clone();
+            var cloneID = country+"Dialog";
+            dialogClone.attr("id", cloneID);
+            $.get(url, function(res){
+                console.log(res[0]);
+                dialogClone.find("h1#countryName").text(res[0]["name"]);
+                dialogClone.find("#countryInfo").append(
+                    "Capital: "+res[0]["capital"]+"<br>"+
+                    "Population: "+res[0]["population"]+"<br>"+
+                    "Currency: "+res[0]["currencies"][0]["name"]
+                );
+            });
+            $(cloneID).show();
+            console.log(dialogClone);
         });
     });
 
